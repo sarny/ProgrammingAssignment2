c1ed6a6a95b8528c3213e1e797f49ed2c27f1cd3
Martin Sarnovsky <martin.sarnovsky@tuke.sk>
2014-11-23 11:39:47 +0100

    Programming assignment 2 commit
    makeCacheMatrix and cacheSolve functions added

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,24 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix function
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+## setting the matrix value
 
+  m <- NULL
+  set <- function(y){
+  x <<- y
+  m <<- NULL
+}
+
+## get the matrix value
+get <- function() x
+
+## set the inverse matrix
+setmatrix <- function(solve) m <<- solve
+getmatrix <- function() m
+list(set=set, get=get,
+   setmatrix=setmatrix,
+   getmatrix=getmatrix)
 }
 
 
@@ -12,4 +26,19 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    
+## getting the inverse matrix
+    m <- x$getmatrix()
+
+## check if it already is copmuted
+    if(!is.null(m)){
+      message("getting cached data")
+      return(m)
+    }
+
+## if not, get the inverse matrix
+    matrix <- x$get()
+    m <- solve(matrix, ...)
+    x$setmatrix(m)
+    m
 }
